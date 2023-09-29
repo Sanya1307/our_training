0050800a6cd8d15f4097408b48a044003635d9e3
Roman <zxcsull83@gmail.com>
2023-09-29 22:44:52 +0400

    commit by roman #4

--- General_site/ToDo_app/models.py
+++ General_site/ToDo_app/models.py
@@ -1,4 +1,10 @@
 from django.db import models
 
 
+class User(models.Model):
+    username = models.CharField('Имя пользователя', max_length=255)
+    email = models.CharField('E-mail пользователя', max_length=255)
+    password = models.CharField('Пароль пользователя', max_length=255)
 
+    def __str__(self):
+        return self.username
--- General_site/ToDo_app/views.py
+++ General_site/ToDo_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import *
-import sqlite3
 
 
 def Home(request):
